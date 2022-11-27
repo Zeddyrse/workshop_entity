fe44dae2cb91245e7a7946466e0206bc7a8208f9
jeremy <jeremy.guimard.pro@gmail.com>
2022-11-27 18:02:27 +0100

    Find all fonctionne

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\Persistence\ManagerRegistry;
 use App\Repository\SquirrelRepository;
+use App\Repository\NutRepository;
 use App\Entity\Squirrel;
 
 
@@ -14,8 +15,11 @@ use App\Entity\Squirrel;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(SquirrelRepository $squirrelRepository): Response
+    public function index(SquirrelRepository $squirrelRepository, NutRepository $nutRepository): Response
     {
-      return $this->render('index.html.twig',['squirrels' => $squirrelRepository->findAll()]);
+      return $this->render('index.html.twig',
+      ['squirrels' => $squirrelRepository->findAll(),
+        'nuts' => $nutRepository->findAll()
+      ]);
     }
 }
